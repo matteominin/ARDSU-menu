dfb95350b90d53776f37f7f69bf5867e000a17d2
matteominin <matteominin@yahoo.it>
2023-05-23 22:01:00 +0200

    added full menù endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const { getPdfUrl, parseData, loadPdf } = require("./parser.js");
+const { parse } = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 const months = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno","luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"];
@@ -8,6 +9,17 @@ const months = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno","lu
 /* REGEX */
 const getLunch = /(?<=P\s+R\s+A\s+N\s+\s+Z\s+O)(.*)(?=C\s+E\s+N\s+A)/gms;
 const getDinner = /(?<=C\s+E\s+N\s+A)(.*)(?=Nei menù)/gms;
+
+app.get("/", async (req, res) => {
+  const url = await getPdfUrl();
+  let month = url.match(/\+\w+\+/g)[0].replace(/\+/g, "");
+  month = months.indexOf(month.toLocaleLowerCase());
+  const buffer = await loadPdf(url);
+  let lunch = await parseData(buffer, getLunch, month);
+  let dinner = await parseData(buffer, getDinner, month);
+
+  res.json({lunch, dinner});
+})
 
 app.get("/lunch", async (req, res) => {
     const url = await getPdfUrl();
